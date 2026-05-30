f69f1c398173e047ddaebd4fd6ab596da1b22599
ShadWeb <shyifou@gmail.com>
2026-05-30 11:42:44 +0330

    start pdf vada

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  devIndicators: false,
+  output: "export",
+  images: { unoptimized: true },
+  trailingSlash: true,
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
